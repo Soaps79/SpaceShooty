6e69e7f6d9ad140eef1db446aff79c9a6e21a6aa
Soaps79 <dansopel@gmail.com>
2021-12-26 23:33:05 -0500

    bouncy asteroid

---
+++ src/SpaceShooty/Assets/Scripts/ScreenWrap.cs
@@ -0,0 +1,13 @@
+﻿using QGame;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ScreenWrap : QScript
+    {
+        void OnBecameInvisible()
+        {
+            transform.SetPositionAndRotation(new Vector3(transform.position.x * -1, transform.position.y * -1, transform.position.z), transform.rotation);
+        }
+    }
+}
---
+++ src/SpaceShooty/Assets/Scripts/Weapons/BaseBullet.cs
@@ -0,0 +1,29 @@
+﻿using QGame;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Assets.Scripts.Weapons
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class BaseBullet : QScript
+    {
+        private Collider2D _collider;
+        public float PushPower;
+
+        void Awake()
+        {
+            _collider = GetComponent<Collider2D>();
+            //_collider.
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.attachedRigidbody != null)
+            {
+                var force = transform.position - other.transform.position;
+                force.Normalize();
+                other.attachedRigidbody.AddForce(-force * PushPower);
+            }
+        }
+    }
+}
