d84ab0447711bf9b1e2b9f73d681ba1f581a0b4b
Soaps79 <dansopel@gmail.com>
2021-12-26 21:24:13 -0500

    Destroy bullets offscreen

---
+++ src/SpaceShooty/Assets/Scripts/DestroyOffScreen.cs
@@ -0,0 +1,12 @@
+﻿using QGame;
+
+namespace Assets.Scripts
+{
+    public class DestroyOffScreen : QScript
+    {
+        void OnBecameInvisible()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
