91f609322579158ce87c80c7bad0a157a5221009
Soaps79 <dansopel@gmail.com>
2021-12-26 22:15:11 -0500

    added single barrel gun

---
+++ src/SpaceShooty/Assets/Scripts/SingleBarrel.cs
@@ -0,0 +1,42 @@
+﻿using QGame;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SingleBarrel : QScript
+    {
+        public GameObject BulletPrefab;
+        public GameObject ExitPoint;
+
+        public float BulletSpeed;
+
+        public float FireDelay;
+        [SerializeField]
+        private float _elapsedSinceFire;
+        private bool _isInFireDelay;
+
+        protected override void OnUpdate()
+        {
+            if (_isInFireDelay)
+            {
+                _elapsedSinceFire += Time.deltaTime;
+                if (_elapsedSinceFire > FireDelay)
+                    _isInFireDelay = false;
+            }
+
+            if (!_isInFireDelay && Input.GetMouseButton(0))
+            {
+                FireBullet();
+            }
+        }
+
+        private void FireBullet()
+        {
+            var bullet = GameObject.Instantiate(BulletPrefab, ExitPoint.transform.position, transform.rotation);
+            var rigidBody = bullet.GetComponent<Rigidbody2D>();
+            rigidBody.AddForce(transform.up * BulletSpeed);
+            _isInFireDelay = true;
+            _elapsedSinceFire = 0.0f;
+        }
+    }
+}
