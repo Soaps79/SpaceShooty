465753479864d9a828d3d08699ddcee996b0a6b4
Soaps79 <dansopel@gmail.com>
2021-12-29 17:18:01 -0500

    Added shotgun

---
+++ src/SpaceShooty/Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,29 @@
+﻿using QGame;
+
+namespace Assets.Scripts
+{
+    // usage is wonky af, fix me when more use cases
+    public class DestroyTimer : QScript
+    {
+        public float Lifetime;
+
+        void Awake()
+        {
+            if (Lifetime != 0.0f)
+            {
+                StartTimer();
+            }
+        }
+
+        public void SetLifetime(float lifetime)
+        {
+            Lifetime = lifetime;
+            StartTimer();
+        }
+
+        public void StartTimer()
+        {
+            StopWatch.AddNode("t", Lifetime).OnTick += () => { Destroy(gameObject); };
+        }
+    }
+}
---
+++ src/SpaceShooty/Assets/Scripts/Weapons/Shotgun.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.Weapons
+{
+    public class Shotgun : BaseGun
+    {
+        public GameObject PelletPrefab;
+        public GameObject ExitPoint;
+
+        public float SprayArc;
+        public int PelletCount;
+        public float PelletLifetime;
+        public float MinSpeed;
+        public float MaxSpeed;
+
+        protected override void Fire()
+        {
+            var exitPos = ExitPoint.transform.position;
+            var delta = SprayArc / PelletCount;
+            var start = 0 - delta * (PelletCount / 2);
+
+            for (var i = 0; i < PelletCount; i++)
+            {
+                var angleOffset = start + delta * i;
+                var angleOffsetVector = new Vector3(0, 0, angleOffset);
+                var pellet = GameObject.Instantiate(PelletPrefab, exitPos, transform.rotation);
+                pellet.transform.Rotate(angleOffsetVector);
+
+                var rigidBody = pellet.GetComponent<Rigidbody2D>();
+                rigidBody.AddForce(pellet.transform.up * GetSpeed());
+
+                var destroyTimer = pellet.GetComponent<DestroyTimer>();
+                destroyTimer.SetLifetime(PelletLifetime);
+            }
+        }
+
+        private float GetSpeed()
+        {
+            return Random.Range(MinSpeed, MaxSpeed);
+        }
+    }
+}
